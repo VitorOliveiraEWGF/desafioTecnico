b12ce204697243136c78c5ae464ee5e66579ea32
VitorOliveiraEWGF <135755701+VitorOliveiraEWGF@users.noreply.github.com>
2024-02-21 13:36:36 -0300

    Atualização

--- src/main/java/com/agenda/repository/LogDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.agenda.repository;
-
-import com.agenda.model.LogData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogDataRepository extends JpaRepository<LogData, Long> {
-}
